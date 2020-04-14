09c852978c0fba13bfedeb1e7a1cda4e96a0f1d5
Max Gontijo de Oliveira <maxopala@gmail.com>
2020-04-14 20:25:45 -0300

    Mais alterações

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main()
 {
+    cout << "*****************************\n";
+    cout << "* Programas das aulas extra *\n";
+    cout << "*****************************\n";
+
     declaracao_variaveis_referencias();
 
     ponteiros();
